0a3563668fdbec6e05070109a2d7364f0d7dcb60
René Klatt <rene.klatt@spryker.com>
2026-06-24 10:11:26 +0200

    Updated doc blocks (#581)
    * Updated doc blocks

--- src/Spryker/Zed/ProductAttributeGui/ProductAttributeGuiConfig.php
+++ src/Spryker/Zed/ProductAttributeGui/ProductAttributeGuiConfig.php
@@ -12,41 +12,57 @@ use Spryker\Zed\Kernel\AbstractBundleConfig;
 class ProductAttributeGuiConfig extends AbstractBundleConfig
 {
     /**
+     * @api
+     *
      * @var string
      */
     public const DEFAULT_LOCALE = '_';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const KEY = 'key';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const IS_SUPER = 'is_super';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ATTRIBUTE_ID = 'attribute_id';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ALLOW_INPUT = 'allow_input';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const INPUT_TYPE = 'input_type';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const ID_PRODUCT_ATTRIBUTE_KEY = 'id_product_attribute_key';
 
     /**
+     * @api
+     *
      * @var string
      */
     public const LOCALE_CODE = 'locale_code';
